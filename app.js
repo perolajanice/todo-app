dece45dd2460c001f6e99c46636607dd29b8722c
Perola Da Cruz <perola.dacruz@talktalkplc.com>
2017-11-04 15:42:05 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = angular.module('ToDo',[]).
+controller('todoController', ['$scope', function($scope){
+
+  $scope.title = 'Todo List App'
+
+  $scope.todos = [{
+    'title': 'Build a todo app', 'done':false
+  }];
+
+  $scope.addTodo = function(){
+    $scope.todos.push({'title': $scope.newTodo, 'done':false})
+    $scope.newTodo = ''
+  };
+
+  $scope.clearCompleted = function(){
+    $scope.todos = $scope.todos.filter(function(item){
+      return !item.done
+    })
+  };
+}])
